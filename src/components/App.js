84958a5fec12d06169f1de7e7824d674a18bf0e0
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-04-19 18:07:59 +0300

    Pass user data and project list to components via props

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,11 @@ function App() {
     <div>
       <NavBar />
       <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
+      <About
+        bio={user.bio}
+        github={user.links.github}
+        linkedin={user.links.linkedin}
+      />
       <ProjectList projects={user.projects} />
     </div>
   );
